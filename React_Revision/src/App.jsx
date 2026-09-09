cb0cba03698a6acf596a09a2517052574101acc0
shanmukatonangi <shanmukatonangi@gmail.com>
2026-09-09 21:51:09 +0530

    usestate intro

--- React_Revision/src/App.jsx
+++ React_Revision/src/App.jsx
@@ -1,52 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Gallery from './components/Gallery'
-import Footer from './components/Footer'
+import React, { useState } from 'react'
 
 const App = () => {
 
-  // let username="shanmukh"
+ // let count=0
+  let [count,setCount]=useState(0)
 
-  // function Hello(){
-  //   console.log("hello")
-  // }
+  function Plus(){
+   // count=count+1
+   setCount(count+1)
+    console.log(count)
+  }
+  //count=0 ->user clicked on plus with the help of setcount now count="shanmukh"
 
-  // function Cliked(name){
-  //   console.log("clicked",name)
-  // }
-  let loggedin=true
-  let user="Shanmukh"
-
-  //and- evrything should be true to return true
-  //or  - one true to return true
-
-
+  
   return (
     <div>
-      {/* <Navbar data={username}/>
-      <Hero />
-      <Gallery data={username} functiondata={Hello} />
-      <Footer /> */}
+      <button onClick={Plus}>+</button>
+      {count}
 
-      {/* <button onClick={()=>Cliked("shanmukh")}>Click me</button> */}
-
-
-
-
-     {/* {!loggedin && <h1>Hello User, Please Login</h1> }
-     {loggedin && <h1>hello {user}, Have a Great day</h1>} */}
-
-      {/* <h1>Hello User, Please Login</h1>
-
-      <h1>hello Shanmukh, Have a Great day</h1> */}
-
-      {loggedin==true ? <h1>hello {user}, Have a Great day</h1> :<h1>Hello User, Please Login</h1> }
-
-
-
-
-
+      {/* //task-> create a event managment system */}
+      
     </div>
   )
 }
