db8ad25bf010138b548dab6bcfc317d9df96cf57
shanmukatonangi <shanmukatonangi@gmail.com>
2026-09-09 21:36:40 +0530

    conditional rendering

--- React_Revision/src/App.jsx
+++ React_Revision/src/App.jsx
@@ -6,17 +6,47 @@ import Footer from './components/Footer'
 
 const App = () => {
 
-  let username="shanmukh"
+  // let username="shanmukh"
 
-  function Hello(){
-    console.log("hello")
-  }
+  // function Hello(){
+  //   console.log("hello")
+  // }
+
+  // function Cliked(name){
+  //   console.log("clicked",name)
+  // }
+  let loggedin=true
+  let user="Shanmukh"
+
+  //and- evrything should be true to return true
+  //or  - one true to return true
+
+
   return (
     <div>
-      <Navbar data={username}/>
+      {/* <Navbar data={username}/>
       <Hero />
       <Gallery data={username} functiondata={Hello} />
-      <Footer />
+      <Footer /> */}
+
+      {/* <button onClick={()=>Cliked("shanmukh")}>Click me</button> */}
+
+
+
+
+     {/* {!loggedin && <h1>Hello User, Please Login</h1> }
+     {loggedin && <h1>hello {user}, Have a Great day</h1>} */}
+
+      {/* <h1>Hello User, Please Login</h1>
+
+      <h1>hello Shanmukh, Have a Great day</h1> */}
+
+      {loggedin==true ? <h1>hello {user}, Have a Great day</h1> :<h1>Hello User, Please Login</h1> }
+
+
+
+
+
     </div>
   )
 }
